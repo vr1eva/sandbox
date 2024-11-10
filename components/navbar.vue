a6bca42fce9b9d0aa2f5ba84b4866a6a1c4645d6
vr1 <hivr1eva@gmail.com>
2024-11-10 09:17:38 -0500

    add icons

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,7 +1,12 @@
 <template>
-    <nav>
-        <NuxtLink to="github.com/vr1eva/sandbox" target="_blank">
-            <nuxt-icon name="github" prefilled />
-        </NuxtLink>
-    </nav>
+    <div class="flex px-2 py-3 items-center bg-[peru]">
+        <h1 class="text-white">Sandbox</h1>
+        <nav class="ml-auto">
+            <NuxtLink class="flex gap-2 items-center justify-center p-2 rounded border-2 hover:bg-white font-mono"
+                to="https://github.com/vr1eva/sandbox" target="_blank">
+                <nuxt-icon width="16" height="16" name="github" prefilled />
+                <span>View source code on GitHub</span>
+            </NuxtLink>
+        </nav>
+    </div>
 </template>
